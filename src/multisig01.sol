3c5377f29b6143ee0f7df8d6876fef8eb8bf8aad
d-smith <doug.smith.mail@gmail.com>
2023-05-09 15:52:27 -0700

    first commit of naive contract

---
+++ src/multisig01.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.8.13;
+
+contract MultiSigSender {
+    // Signers
+    address[] signers;
+
+    // Spend transaction
+    struct SpendTxn {
+        address recipient;
+        uint256 amount;
+        mapping(address => bool) approvals;
+        uint256 approvalCount;
+    }
+
+    // Spend transactions
+    SpendTxn[] public spendTransactions;
+
+    
+
+    constructor(address[] memory walletSigners) {
+        require(walletSigners.length == 3);
+        signers = walletSigners;
+    }
+
+    function  isSigner(address s)  view public returns (bool)  {
+        return signers[0] == s || signers[1] == s 
+        || signers[2] ==  s;
+    }
+
+    receive() external payable {}
+    
+    function submitSpend(address recipient, uint256 amount) public  {
+        uint256 index = spendTransactions.length;
+
+        spendTransactions.push();
+        spendTransactions[index].recipient = recipient;
+        spendTransactions[index].amount = amount;
+    } 
+
+    function approveSpend(uint256 transactionNo) public {
+        SpendTxn storage txn = spendTransactions[transactionNo];
+        txn.approvals[msg.sender] = true;
+        txn.approvalCount += 1;
+    }
+
+    function executeTransaction(uint256 transactionNo) public {
+        SpendTxn storage txn = spendTransactions[transactionNo];
+        (bool success, ) = txn.recipient.call{value: txn.amount}("");
+        require(success, "execute transaction failed");
+    }
+}
